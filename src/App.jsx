70a6975cbc56657e813d7bc9ef6da988180ce418
Nk2603 <thakrannisha2005@gmail.com>
2024-06-14 21:31:52 +0530

    dynamic card

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,18 @@ import Navbar from './components/Navbar';
 import Slidebar from './components/Slidebar'; 
 import Card from './components/Card';
 import Contact from './components/Contact';
+import Card1 from './components/Card1';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Navbar/>
+    <Card1/>
+      {/* <Navbar/>
       <Slidebar/>
       <Card/>
-      <Contact/>
+      <Contact/> */}
     </>
   )
 }
